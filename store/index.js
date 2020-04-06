a8f2a72d8db03d9285b9c46bcd7ce62aa74c42b2
damienmontastier <montastier.damien@gmail.com>
2020-04-06 12:54:10 +0200

    wip config.yml

--- store/index.js
+++ store/index.js
@@ -7,14 +7,18 @@ export const mutations = {}
 export const getters = {}
 
 export const actions = {
-  async nuxtServerInit({ commit }) {
-    const files = await require.context('~/assets/content/', false, /\.json$/)
-    const blogPosts = files.keys().map((key) => {
-      const res = files(key)
-      res.slug = key.slice(2, -5)
-      return res
-    })
-    console.log(blogPosts)
-    await commit('setBlogPosts', blogPosts)
-  }
+  //   async nuxtServerInit({ commit }) {
+  //     let files = await require.context(
+  //       '~/assets/content/blog/',
+  //       false,
+  //       /\.json$/
+  //     )
+  //     const blogPosts = files.keys().map((key) => {
+  //       const res = files(key)
+  //       res.slug = key.slice(2, -5)
+  //       return res
+  //     })
+  //     console.log(blogPosts)
+  //     await commit('setBlogPosts', blogPosts)
+  //   }
 }
