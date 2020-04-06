466e6f4230f4562acd192e82417a0e156c47d739
damienmontastier <montastier.damien@gmail.com>
2020-04-06 14:05:07 +0200

    config.yml

--- store/index.js
+++ store/index.js
@@ -1,15 +1,21 @@
 export const strict = false
 
-export const state = () => ({})
+export const state = () => ({
+  levels: []
+})
 
-export const mutations = {}
+export const mutations = {
+  setLevelsContent(state, levels) {
+    state.levels = levels
+  }
+}
 
 export const getters = {}
 
 export const actions = {
   async nuxtServerInit({ commit }) {
     const files = await require.context(
-      '~/assets/content/about',
+      '~/assets/content/levels',
       false,
       /\.json$/
     )
