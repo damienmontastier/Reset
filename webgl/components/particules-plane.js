33afb7ac28eb55d99f39a339521ca1f38d4051f7
Clément Roche <rchclement@gmail.com>
2020-05-07 10:44:52 +0200

    dots gui

--- webgl/components/particules-plane.js
+++ webgl/components/particules-plane.js
@@ -1,6 +1,8 @@
 import vertexShader from '@/webgl/materials/particules/vertex.glsl'
 import fragmentShader from '@/webgl/materials/particules/fragment.glsl'
 
+import useGUI from '@/hooks/use-gui'
+
 export default class PatriculesPlane extends THREE.Object3D {
   constructor() {
     super()
@@ -34,9 +36,39 @@ export default class PatriculesPlane extends THREE.Object3D {
     // this.mesh = new THREE.Points(this.geometry, this.material)
     this.mesh = new THREE.Mesh(this.geometry, this.material)
     this.add(this.mesh)
+
+    this.initGUI()
   }
 
   update(clock) {
     this.material.uniforms.uTime.value = clock.time * 0.25
+  }
+
+  initGUI() {
+    const GUI = useGUI()
+    const dotsGUI = GUI.addFolder('dots pattern')
+    dotsGUI
+      .add(this.material.uniforms.uDotsFrenquency, 'value')
+      .min(0)
+      .max(1000)
+      .name('dots frequency')
+    dotsGUI
+      .add(this.material.uniforms.uDotsRadius, 'value')
+      .min(0)
+      .max(1)
+      .step(0.01)
+      .name('dots radius')
+    dotsGUI
+      .add(this.material.uniforms.uNoiseAmplitude, 'value')
+      .min(0)
+      .max(0.1)
+      .step(0.001)
+      .name('noise amplitude')
+    dotsGUI
+      .add(this.material.uniforms.uNoiseFrequency, 'value')
+      .min(0)
+      .max(20)
+      .step(0.1)
+      .name('noise frequency')
   }
 }
