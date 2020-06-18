611d52a65e958f9fd6509be9fe3618bdd66bba8f
clementroche <rchclement@gmail.com>
2020-06-19 00:59:20 +0200

    solutions svg

--- components/game/solutions/solutions.vue
+++ components/game/solutions/solutions.vue
@@ -32,9 +32,9 @@
       </aside>
       <div class="solutions__main">
         <div class="solutions__list">
-          <scroller :scrollable="!modalOpened" :draggable="!modalOpened">
-            <div class="solutions__main__grid">
-              <solution
+          <!-- <scroller :scrollable="!modalOpened" :draggable="!modalOpened"> -->
+          <div class="solutions__main__grid">
+            <!-- <solution
                 @click.native="selectSolution(solution)"
                 v-for="(solution, i) in orderedSolutions"
                 :key="i"
@@ -42,30 +42,30 @@
                 :unlocked="solution.unlocked"
                 :class="{ 'solutions__solution--unlocked': solution.unlocked }"
                 class="solutions__solution"
-              />
-            </div>
-          </scroller>
+              /> -->
+          </div>
+          <!-- </scroller> -->
         </div>
-        <modal
+        <!-- <modal
           v-if="modalOpened"
           :solution="selectedSolution"
           @close="modalOpened = false"
-        />
+        /> -->
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import Solution from './solution'
-import Modal from './modal'
+// import Solution from './solution'
+// import Modal from './modal'
 export default {
   components: {
-    Modal,
-    Solution,
+    // Modal,
+    // Solution,
     HardDriveSvg: () => import('@/components/svg/hard-drive'),
-    CrossSvg: () => import('@/components/svg/cross'),
-    Scroller: () => import('@/components/components/scroller')
+    CrossSvg: () => import('@/components/svg/cross')
+    // Scroller: () => import('@/components/components/scroller')
   },
   data() {
     return {
