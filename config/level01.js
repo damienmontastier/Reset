452a19a2bc9a909816d81877ce8b1be2eef11b4b
Mekwek <65230646+Mekwek@users.noreply.github.com>
2020-06-28 18:45:33 +0200

    Config caméras

--- config/level01.js
+++ config/level01.js
@@ -8,12 +8,20 @@ export default {
   },
   cameras: {
     default: {
-      normalized_angle: new THREE.Vector3(1.1, 6, 6).normalize(),
+      normalized_angle: new THREE.Vector3(0.13, 0.7, 0.7),
       distance: 7
     },
+    appear: {
+      normalized_angle: new THREE.Vector3(1.05, 0.55, 0.7),
+      distance: 4
+    },
     close_up: {
-      normalized_angle: new THREE.Vector3(-1.1, 6, 6).normalize(),
-      distance: 3.5
+      normalized_angle: new THREE.Vector3(0.2, 0.3, 0.7),
+      distance: 4
+    },
+    goal: {
+      normalized_angle: new THREE.Vector3(0.13, 0.7, 0.7),
+      distance: 5
     }
   }
 }
