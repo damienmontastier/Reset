5b4441959790c29c9f299cbd6520bdf315539b53
clementroche <rchclement@gmail.com>
2020-06-29 22:47:07 +0200

    gameplay

---
+++ pages/test.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.$router.push({ name: 'slug', params: { slug: 'infinite-scroll' } })
+  }
+}
+</script>
+
+<style></style>
