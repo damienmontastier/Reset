a21145df5fd4e30e2d45fb723ce0a36bc547a37a
Mekwek <65230646+Mekwek@users.noreply.github.com>
2020-06-29 16:53:54 +0200

    config dotted pattern

--- config/introduction.js
+++ config/introduction.js
@@ -1,7 +1,7 @@
 export default {
   dots: {
-    dotsRadius: 0.2,
-    dotsFrenquency: 100,
+    dotsRadius: 0.08,
+    dotsFrenquency: 71,
     noiseAmplitude: 0.0025,
     noiseFrequency: 10,
     color: 0x000000
